1cd8f6807c167398c1eb05f021c3ec6ae523f433
Grant <gmorela@purdue.edu>
2026-03-08 20:12:11 +0000

    added multiply, divide, std, kph converter, percentage

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ def add(a: str, b: str):
         b = float(b)
     except ValueError:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
-    return {"result": a + b}
+    
+    result = a + b
+    return {
+        "operation": "add",
+        "a": a,
+        "b": b,
+        "result": result}
+
 
 @app.get("/subtract/{a}/{b}", status_code=200)
 def subtract(a: str, b: str):
@@ -45,4 +52,151 @@ def subtract(a: str, b: str):
         b = float(b)
     except ValueError:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
-    return {"result": a - b}
+    
+    result = a - b
+    return {
+        "operation": "subtract",
+        "a": a,
+        "b": b,
+        "result": result}
+
+
+@app.get("/multiply/{a}/{b}", status_code=200)
+def multiply(a: str, b: str):
+    """
+    multiply two numbers together.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
+    
+    result = a * b
+    return {
+        "operation": "multiply",
+        "a": a,
+        "b": b,
+        "result": result}
+
+
+@app.get("/divide/{a}/{b}", status_code=200)
+def divide(a: str, b: str):
+    """
+    divide two numbers together. second number divides the first.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
+    
+    if b == 0:
+        return{"error": "division by 0 is not possible, please choose a different number"}
+    else:
+        result = a / b
+    return {
+        "operation": "divide",
+        "a": a,
+        "b": b,
+        "result": result}
+
+
+@app.get("/KPHtoMPH/{a}", status_code=200)
+def KPHtoMPH(a: str):
+    """
+    converts kilometers per hour to miles per hour.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try:
+        a = float(a)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
+    
+    result = round(a * 0.621371, 0)
+    return {
+        "operation": "KPHtoMPH",
+        "a": a,
+        "result": result}
+
+
+@app.get("/standardDeviation/{a}/{b}/{c}", status_code=200)
+def standardDeviation(a: str, b: str, c: str):
+    """
+    calculates the standard deviation of 3 numbers.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    - c: third number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="a, b and c must be numbers")
+    
+    mean = (a + b + c) / 3
+    variance = ((a - mean) ** 2 + (b - mean) ** 2 + (c - mean) ** 2) / 3
+    std = variance ** 0.5
+
+    result = std
+    return {
+        "operation": "standardDeviation",
+        "a": a,
+        "b": b,
+        "c": c,
+        "result": result}
+
+
+@app.get("/percentage/{a}/{b}", status_code=200)
+def percentage(a: str, b: str):
+    """
+    finds the percentage of the first number compared to the second number.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try:
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="both a and b must be numbers")
+    
+    if b == 0:
+        return{"error": "division by 0 is not possible, please choose a different number"}
+    else:
+        result = a / b * 100
+    return {
+        "operation": "percentage",
+        "a": a,
+        "b": b,
+        "result": result}
